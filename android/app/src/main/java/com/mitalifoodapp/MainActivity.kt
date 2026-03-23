be23995eddab4e6158224d8c67b3e0ca36913f7b
mitesh <gehlotmitesh63@gmail.com>
2026-03-24 02:46:17 +0530

    feat: setup ci /cd

--- android/app/src/main/java/com/mitalifoodapp/MainActivity.kt
+++ android/app/src/main/java/com/mitalifoodapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.foodapp
+package com.mitalifoodapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/mitalifoodapp/MainApplication.kt
+++ android/app/src/main/java/com/mitalifoodapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.foodapp
+package com.mitalifoodapp
 
 import android.app.Application
 import com.facebook.react.PackageList
